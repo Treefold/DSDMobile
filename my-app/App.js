cde9de9524fd7b09d79d31263c184783709aa269
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-05-18 23:26:32 +0300

    login frontend

--- my-app/App.js
+++ my-app/App.js
@@ -1,46 +1,14 @@
-import { createContext, useContext, useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import styled from 'styled-components/native'
+import React from 'react';
 
+// screens
 import Login from './screens/Login';
 
-const Column = styled(View)`
-  flex-direction: column;
-`;
 
-const Row = styled(View)`
-  flex-direction: row;
-`;
-
-const Container = styled(View)`
-  flex: 1;
-  align-items: center;
-  padding-top: 50px;
-  background-color: #f7e6b7;
-`;
-
-const CustomTextInput = styled(TextInput)`
-  width: 150px;
-  height: 30px;
-  background-color: #d4be83;
-  border-radius: 5px;
-  padding-left: 5px;
-`;
-
-const CustomButton = styled(TouchableOpacity)`
-  width: 80px;
-  height: 30px;
-  background-color: #d4be83;
-  border-radius: 5px;
-  align-items: center;
-  justify-content: center;
-  margin-left: 10px;
-`;
-
-export default function App() {
-
-  const [text, setText] = useState('');
-
+const App = () => {
   return (
     <Login />
+  );
 }
+
+
+export default App;
--- my-app/screens/Login.js
+++ my-app/screens/Login.js
@@ -1,16 +1,102 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Formik } from 'formik';
+import { Fontisto, Ionicons, Octicons } from '@expo/vector-icons';
 import {
-    StyledContainer, InnerContainer, PageLogo, PageTitle
-} from './../components/styles'
+    Colors,
+    StyledContainer, InnerContainer, 
+    PageLogo, PageTitle, SubTitle, 
+    StyledFormArea, StyledInputLabel, StyledTextInput, StyledButton, ButtonText,
+    LeftIcon, RightIcon,
+    MsgBox, Line,
+    ExtraView, ExtraText, TextLink, TextLinkContent,
+} from './../components/styles';
+
+const { brand, darkLight, primary } = Colors;
 
 const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+
     return (
         <StyledContainer>
+            <StatusBar style='dark' />
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require('./../assets/img/Logo.png')} />
                 <PageTitle>JustUs</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+
+                        <MyTextInput 
+                            label="Email Address" 
+                            icon="mail" 
+                            placeholder="example@mail.com" 
+                            placeholderTextColor={darkLight}
+                            onChange={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType="email-address"
+                        />
+
+                        <MyTextInput 
+                            label="Password" 
+                            icon="lock" 
+                            placeholder="********" 
+                            placeholderTextColor={darkLight}
+                            onChange={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            secureTextEntry={hidePassword}
+                            isPassword={true}
+                            hidePassword={hidePassword}
+                            setHidePassword={setHidePassword}
+                        />
+
+                        <MsgBox>...</MsgBox>
+                        <StyledButton onPress={handleSubmit}>
+                            <ButtonText>Login </ButtonText>
+                        </StyledButton>
+                        <Line />
+                        <StyledButton google={true} onPress={handleSubmit}>
+                            <Fontisto name="google" color={primary} size={25} />
+                            <ButtonText google={true}>Sigh in with google</ButtonText>
+                        </StyledButton>
+                        <ExtraView>
+                            <ExtraText>Don't have an account already?</ExtraText>
+                            <TextLink>
+                                <TextLinkContent>Signup</TextLinkContent>
+                            </TextLink>
+                        </ExtraView>
+
+                    </StyledFormArea>)}
+                </Formik>
+
             </InnerContainer>
         </StyledContainer>
+    );
+}
+
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+                <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                    <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} /> 
+                </RightIcon>   
+            )}
+        </View>
     );
 }
 

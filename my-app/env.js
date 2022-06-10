88ab316f3e0be871147c9486810a44069e555a32
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-06-11 00:01:02 +0300

    real login

---
+++ my-app/env.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default {
+    endpoint: "http://192.168.50.28:8080/api/",
+    urlBasic: "http://192.168.50.28",
+};
--- my-app/screens/Login.js
+++ my-app/screens/Login.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, View, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Formik } from 'formik';
+import axios from 'axios';
 import { Fontisto, Ionicons, Octicons } from '@expo/vector-icons';
-import KeyboardAvoindingWrapper from '../components/KeyboardAvoindingWrapper';
+import env from "./../env";
+
+import KeyboardAvoindingWrapper from './../components/KeyboardAvoindingWrapper';
 import {
     Colors,
     StyledContainer, InnerContainer, 
@@ -16,92 +19,136 @@ import {
 
 const { brand, darkLight, primary } = Colors;
 
+// axios.post(
+//     "http://" + "192.168.50.28" + ":8080/api/sessions", 
+//     {email:"test@example.com", password:"Password456!"}
+// ).then((response) => {
+//     console.log(response)
+// }).catch((error) => {
+//     console.log("HandleLoginError: ", error);
+// });
+
 const Login = ({navigation}) => {
-    const [hidePassword, setHidePassword] = useState(true);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState(); 
 
-    return (
-        <KeyboardAvoindingWrapper>
-            <StyledContainer>
-                <StatusBar style='dark' />
-                <InnerContainer>
-                    <PageLogo resizeMode="cover" source={require('./../assets/img/Logo.png')} />
-                    <PageTitle>JustUs</PageTitle>
-                    <SubTitle>Account Login</SubTitle>
+  const handleMessage = (message, type = 'FAILED') => {
+      setMessage (message);
+      setMessageType (type);
+  }
 
-                    <Formik
-                        initialValues={{ email: '', password: '' }}
-                        onSubmit={(values) => {
-                            console.log(values);
-                            navigation.navigate("Welcome");
-                        }}
-                    >
-                        {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+  const handleLogin = (credentials, setSubmitting) => {
+    handleMessage(null);
 
-                            <MyTextInput 
-                                label="Email Address" 
-                                icon="mail" 
-                                placeholder="example@mail.com" 
-                                placeholderTextColor={darkLight}
-                                onChange={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                value={values.email}
-                                keyboardType="email-address"
-                            />
+    const url = env.endpoint + 'sessions';
 
-                            <MyTextInput 
-                                label="Password" 
-                                icon="lock" 
-                                placeholder="********" 
-                                placeholderTextColor={darkLight}
-                                onChange={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                secureTextEntry={hidePassword}
-                                isPassword={true}
-                                hidePassword={hidePassword}
-                                setHidePassword={setHidePassword}
-                            />
+    axios.post(url, credentials).then((response) => {
+        navigation.navigate('Welcome', response.data);
+        setSubmitting(false);
+    }).catch((error) => {
+        handleMessage(error?.response?.data || 'An error occured', 'FAILED');
+        setSubmitting(false);
+    });
+  };
+  
+  return (    
+    <KeyboardAvoindingWrapper>
+      <StyledContainer>
+          <StatusBar style='dark' />
+          <InnerContainer> 
+            <PageLogo resizeMode="cover" source={require('./../assets/img/Logo.png')} />
+            <PageTitle>JustUs</PageTitle>
+            <SubTitle>Account Login</SubTitle>
 
-                            <MsgBox>...</MsgBox>
-                            <StyledButton onPress={handleSubmit}>
-                                <ButtonText>Login</ButtonText>
-                            </StyledButton>
-                            <Line />
-                            <StyledButton google={true} onPress={handleSubmit}>
-                                <Fontisto name="google" color={primary} size={25} />
-                                <ButtonText google={true}>Sigh in with google</ButtonText>
-                            </StyledButton>
-                            <ExtraView>
-                                <ExtraText>Don't have an account already? </ExtraText>
-                                <TextLink onPress={() => {navigation.navigate("Signup");}}>
-                                    <TextLinkContent>Signup</TextLinkContent>
-                                </TextLink>
-                            </ExtraView>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                    if (!(values.email?.length > 0 && values.password?.length > 0 )) {
+                        handleMessage("Please fill all fields");
+                        setSubmitting(false);
+                        return;
+                    }
+                    
+                    handleLogin(values, setSubmitting);
+                    setSubmitting(false);
+                }}
+            >
+            {({ handleChange, handleBlur, handleSubmit, values, isSubmitting }) => (
+              <StyledFormArea>
+                <MyTextInput
+                    label="Email Address" 
+                    icon="mail" 
+                    placeholder="example@mail.com" 
+                    placeholderTextColor={darkLight}
+                    onChangeText={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                    value={values.email}
+                    keyboardType="email-address"
+                />
+                <MyTextInput
+                    label="Password" 
+                    icon="lock" 
+                    placeholder="********" 
+                    onChangeText={handleChange('password')}
+                    onBlur={handleBlur('password')}
+                    value={values.password}
+                    secureTextEntry={hidePassword}
+                    isPassword={true}
+                    hidePassword={hidePassword}
+                    setHidePassword={setHidePassword}
+                />
 
-                        </StyledFormArea>)}
-                    </Formik>
+                <MsgBox type={messageType}>{message}</MsgBox>
+                <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Login</ButtonText>
+                </StyledButton>
+                
+                {/* {!isSubmitting && <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Login</ButtonText>
+                </StyledButton>}
 
-                </InnerContainer>
-            </StyledContainer>
-        </KeyboardAvoindingWrapper>
-    );
+                {isSubmitting && <StyledButton disable={true}>
+                    <ActivityIndicator size="small" color={primary} />
+                </StyledButton>} */}
+
+                <Line />
+                <StyledButton google={true} onPress={handleSubmit}>
+                    <Fontisto name="google" color={primary} size={25} />
+                    <ButtonText google={true}>Sigh in with google</ButtonText>
+                </StyledButton>
+                <ExtraView>
+                    <ExtraText>Don't have an account already? </ExtraText>
+                    <TextLink onPress={() => {navigation.navigate("Signup");}}>
+                        <TextLinkContent>Signup</TextLinkContent>
+                    </TextLink>
+                </ExtraView>
+                
+
+              </StyledFormArea>)}
+            </Formik> 
+
+        </InnerContainer>
+      </StyledContainer>
+    </KeyboardAvoindingWrapper>
+ );
 }
 
 const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
-    return(
-        <View>
-            <LeftIcon>
-                <Octicons name={icon} size={30} color={brand} />
-            </LeftIcon>
-            <StyledInputLabel>{label}</StyledInputLabel>
-            <StyledTextInput {...props} />
-            {isPassword && (
-                <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-                    <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} /> 
-                </RightIcon>   
-            )}
-        </View>
-    );
+  return(
+      <View>
+          <LeftIcon>
+              <Octicons name={icon} size={30} color={brand} />
+          </LeftIcon>
+          <StyledInputLabel>{label}</StyledInputLabel>
+          <StyledTextInput {...props} />
+          {isPassword && (
+              <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                  <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} /> 
+              </RightIcon>   
+          )}
+      </View>
+  );
 }
-
+  
 export default Login;

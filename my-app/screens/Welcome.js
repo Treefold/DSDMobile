e16bb4ba9f34b636ab42dfac6a65c560cedc22c5
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-06-11 19:21:34 +0300

    add days until your birthday

--- my-app/screens/Welcome.js
+++ my-app/screens/Welcome.js
@@ -14,8 +14,23 @@ import {
 const Welcome = () => {
 
     const {storedCredentials, setStoredCredentials} = useContext(CredentialsContext);
-    const {name, email, photoUrl} = storedCredentials;
+    const {name, email, photoUrl, dateOfBirth} = storedCredentials;
     const avatarSource = photoUrl ? {uri: photoUrl} : require('./../assets/img/Logo.png');
+
+    const today = new Date();
+    const startBirthday = new Date(dateOfBirth);
+    startBirthday.setFullYear(today.getFullYear())
+    const endBirthday = new Date(startBirthday);
+    endBirthday.setDate(endBirthday.getDate() + 1)
+
+    if (today >= endBirthday) {
+        // next year :(
+        startBirthday.setFullYear(startBirthday.getFullYear() + 1);
+    }
+    const daysToMyNextBirthday = Math.ceil((startBirthday - today) / (1000 * 3600 * 24));
+    const isMyBirthday = (daysToMyNextBirthday == 0);
+    
+    console.log(daysToMyNextBirthday)
 
     return (
         <>
@@ -28,6 +43,22 @@ const Welcome = () => {
                     <SubTitle welcome={true}>{name || 'Test'}</SubTitle>
                     <SubTitle welcome={true}>{email || 'test@mail.com'}</SubTitle>
                     <Avatar resizeMode="cover" source={avatarSource} />
+
+                    {!dateOfBirth  && (
+                        <SubTitle welcome={true}>
+                            Birthdate unknown, so happy birthday anyway!
+                        </SubTitle>
+                    )}
+                    {dateOfBirth && !isMyBirthday && (
+                        <SubTitle welcome={true}>
+                            Your birthday is in {daysToMyNextBirthday} {daysToMyNextBirthday > 1 ? 'days' : 'day' }
+                        </SubTitle>
+                    )}
+                    {dateOfBirth && isMyBirthday && (
+                        <SubTitle welcome={true}>
+                            Happy birthday!
+                        </SubTitle>
+                    )}
 
                     <Line />
                     <StyledButton onPress={() => {
